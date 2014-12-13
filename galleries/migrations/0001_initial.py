9ce31b3067fe5f6680a24ed847f9f43120bb8434
Chris Aliipule <chris@techiniku.com>
2014-12-13 04:25:12 -1000

    Fix migration

--- galleries/migrations/0001_initial.py
+++ galleries/migrations/0001_initial.py
@@ -16,10 +16,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
-                ('description', models.CharField(max_length=255)),
+                ('description', models.CharField(max_length=255, blank=True)),
             ],
             options={
-                'db_table': 'gallery',
+                'db_table': 'galleries',
                 'verbose_name_plural': 'galleries',
             },
             bases=(models.Model,),
@@ -28,40 +28,21 @@ class Migration(migrations.Migration):
             name='Image',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('image', models.ImageField(height_field=b'original_height', width_field=b'original_width', max_length=255, upload_to=b'')),
+                ('image', models.ImageField(height_field=b'original_height', width_field=b'original_width', max_length=255, upload_to=galleries.models.gallery_upload_path)),
                 ('original_width', models.IntegerField()),
                 ('original_height', models.IntegerField()),
-                ('caption', models.CharField(max_length=100)),
+                ('caption', models.CharField(max_length=100, blank=True)),
                 ('position', models.IntegerField()),
                 ('gallery', models.ForeignKey(to='galleries.Gallery')),
             ],
             options={
                 'ordering': ('position',),
+                'db_table': 'gallery_images',
             },
             bases=(models.Model,),
         ),
         migrations.AlterUniqueTogether(
             name='image',
             unique_together=set([('gallery', 'position')]),
-        ),
-        migrations.AlterModelTable(
-            name='gallery',
-            table='galleries',
-        ),
-        migrations.AlterModelTable(
-            name='image',
-            table='gallery_images',
-        ),
-        migrations.AlterField(
-            model_name='image',
-            name='caption',
-            field=models.CharField(max_length=100, blank=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='image',
-            name='image',
-            field=models.ImageField(height_field=b'original_height', width_field=b'original_width', max_length=255, upload_to=galleries.models.gallery_upload_path),
-            preserve_default=True,
         ),
     ]
